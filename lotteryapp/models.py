b9fa38c1e54d05661481192226434013ac6da599
with1015 <with1015@unist.ac.kr>
2020-10-01 10:13:30 +0900

    더미 모델로 개발중

--- lotteryapp/models.py
+++ lotteryapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -12,3 +13,35 @@ from django.db import models
 # 품목 : 부족한 품목 or 남는 품목
 # 수량 : 부족한 수량 or 남는 수량
 # 유효기간 : 만료되는 시간
+
+
+"""
+    Dummy model request/share class 입니다.
+    임시로 내용을 모두 text나 integer로 저장하고 로직을 실행할 목적으로 사용합니다.
+    이 후 로직이 완성되면 model을 고칠 예정입니다.
+"""
+
+class DummyRequest(models.Model):
+
+    location    = models.CharField(max_length=100)              # 요청 장소
+    contents    = models.TextField(blank=True)                  # 본문 내용
+    minute      = models.IntegerField(null=True)                # 만료 시간 (일단은 분 단위)
+    item        = models.CharField(max_length=50, blank=True)   # 요청 품목 이름
+    item_num    = models.IntegerField(null=True)                # 요청 품목 개수
+    dates       = models.DateField(auto_now=True)               # 게시 날짜
+
+    def __str__ (self):
+        return self.location
+
+
+class DummyShare(models.Model):
+
+    location    = models.CharField(max_length=100)  # 공유 장소
+    contents    = models.TextField(blank=True)      # 본문 내용
+    minute      = models.IntegerField()             # 만료 시간 (분 단위)
+    item        = models.CharField(max_length=50)   # 공유 품목 이름
+    item_num    = models.IntegerField()             # 요청 품목 개수
+    dates       = models.DateField(auto_now=True)   # 게시 날짜
+
+    def __str__ (self):
+        return self.location
--- user/models.py
+++ user/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 # 안녕하세요 작업을 다했습니다
+
+class User(models.Model):
+
+    user_id = models.CharField(max_length=20)
+    user_location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_id
